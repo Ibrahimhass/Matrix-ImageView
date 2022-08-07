3de5507bdaa02ffddd2c3711129eff4b27974c80
Ibrahim Hassan <mdibrahimhassan@gmail.com>
2022-08-07 10:32:31 +0530

    Initial commit

--- MatrixFace/ViewController.swift
+++ MatrixFace/ViewController.swift
@@ -9,11 +9,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var matrixFaceImageView: MatrixFaceImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        matrixFaceImageView.startEffect()
     }
-
-
 }
 
